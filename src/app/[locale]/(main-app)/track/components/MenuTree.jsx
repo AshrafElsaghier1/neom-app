779a30e28bde39176cfd7851e2141d209dced707
ashraf <egypt.team.dev11@gmail.com>
2025-11-30 10:44:44 +0200

    enhance ui

--- src/app/[locale]/(main-app)/track/components/MenuTree.jsx
+++ src/app/[locale]/(main-app)/track/components/MenuTree.jsx
@@ -21,12 +21,13 @@ const MenuTree = ({ setLastSelectedSerial }) => {
   return (
     <>
       <aside
-        className={`fixed right-0 top-[72px] h-[calc(100vh-72px)] w-[350px] z-6 
+        className={`fixed right-0 top-[72px] h-[calc(100vh-72px)] w-[365px] z-6 
        border-l border-sidebar-border shadow-xl transform 
           transition-transform duration-300 ease-in-out p-4 bg-sidebar/70  dark:bg-sidebar/95  backdrop-blur-lg
-          ${isMenuOpen
-            ? "translate-x-0"
-            : "rtl:-translate-x-full translate-x-full"
+          ${
+            isMenuOpen
+              ? "translate-x-0"
+              : "rtl:-translate-x-full translate-x-full"
           }`}
       >
         <div className="flex  gap-2 flex-col">
@@ -37,7 +38,10 @@ const MenuTree = ({ setLastSelectedSerial }) => {
           />
 
           <div className="max-w-full ">
-            <VehicleTree statusFilter={vehicleStatus} onLastSelectedSerialChange={handleLastSelectedSerialChange} />
+            <VehicleTree
+              statusFilter={vehicleStatus}
+              onLastSelectedSerialChange={handleLastSelectedSerialChange}
+            />
           </div>
         </div>
       </aside>
@@ -45,9 +49,10 @@ const MenuTree = ({ setLastSelectedSerial }) => {
       <button
         onClick={() => setIsMenuOpen(!isMenuOpen)}
         className={` cursor-pointer fixed top-20 z-6 p-2 rounded-lg transition-all duration-300 ease-in-out shadow-lg 
-          ${isMenuOpen
-            ? " bg-sidebar/80 backdrop-blur-lg  right-[349px] rounded-r-none"
-            : "bg-main text-white right-4"
+          ${
+            isMenuOpen
+              ? " bg-sidebar/80 backdrop-blur-lg  right-[349px] rounded-r-none"
+              : "bg-main text-white right-4"
           }
         `}
       >
