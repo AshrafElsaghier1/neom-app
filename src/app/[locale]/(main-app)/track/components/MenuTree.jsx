97c74c1cb64abdf0bd3b568312f283d84d3715a1
ashraf <egypt.team.dev11@gmail.com>
2025-12-02 11:03:40 +0200

    remove useless state

--- src/app/[locale]/(main-app)/track/components/MenuTree.jsx
+++ src/app/[locale]/(main-app)/track/components/MenuTree.jsx
@@ -6,7 +6,6 @@ import VehicleTree from "./VehicleTree";
 const MenuTree = ({ setLastSelectedSerial }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(true);
   const [vehicleStatus, setVehicleStatus] = useState("all");
-  const [lastSelectedSerial, setLocalLastSelectedSerial] = useState(null);
 
   const handleStatusChange = (newStatus) => {
     setVehicleStatus(newStatus !== "all" ? +newStatus : "all");
@@ -14,7 +13,6 @@ const MenuTree = ({ setLastSelectedSerial }) => {
 
   // Handler for tree selection change
   const handleLastSelectedSerialChange = (serial) => {
-    setLocalLastSelectedSerial(serial);
     if (setLastSelectedSerial) setLastSelectedSerial(serial);
   };
 
@@ -24,10 +22,9 @@ const MenuTree = ({ setLastSelectedSerial }) => {
         className={`fixed right-0 top-[72px] h-[calc(100vh-72px)] w-[365px] z-6 
        border-l border-sidebar-border shadow-xl transform 
           transition-transform duration-300 ease-in-out p-4 bg-sidebar/70  dark:bg-sidebar/95  backdrop-blur-lg
-          ${
-            isMenuOpen
-              ? "translate-x-0"
-              : "rtl:-translate-x-full translate-x-full"
+          ${isMenuOpen
+            ? "translate-x-0"
+            : "rtl:-translate-x-full translate-x-full"
           }`}
       >
         <div className="flex  gap-2 flex-col">
@@ -49,10 +46,9 @@ const MenuTree = ({ setLastSelectedSerial }) => {
       <button
         onClick={() => setIsMenuOpen(!isMenuOpen)}
         className={` cursor-pointer fixed top-20 z-6 p-2 rounded-lg transition-all duration-300 ease-in-out shadow-lg 
-          ${
-            isMenuOpen
-              ? " bg-sidebar/80 backdrop-blur-lg  right-[364px] rounded-r-none"
-              : "bg-main text-white right-4"
+          ${isMenuOpen
+            ? " bg-sidebar/80 backdrop-blur-lg  right-[364px] rounded-r-none"
+            : "bg-main text-white right-4"
           }
         `}
       >
